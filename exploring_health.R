19c0a7641999c1be426eae41c995a4ba88f043cf
Bridget Connelly <bfc27@georgetown.edu>
2021-07-19 08:27:27 -0400

    Updated analysis
    Adding graphics for questions 2 & 3

--- exploring_health.R
+++ exploring_health.R
@@ -13,9 +13,11 @@ library(tidyverse)
 library(janitor)
 library(openxlsx)
 library(caret)
+library(factoextra)
 
 # Import data
 goal3_raw <- read.xlsx('Health Data\\Goal3.xlsx')
+countries <- read.xlsx('Country_List.xlsx')
 
 # Convert all data to a percent out of 100
   # Not percentage-able: 3.2.1, 3.2.2, 3.3.5, 3.4.1, 3.4.2, 53.5.2?, 3.8.1, 3.b.2
@@ -42,19 +44,17 @@ goal3 <- goal3 %>%
                            SeriesCode == 'SH_HLF_EMED' ~ 100 - Value,
                            TRUE ~ Value))
 
+# Keep only the indicators with a percent value
 goal3_perc <- goal3 %>% 
   filter(!(Units %in% c("NUMBER", "INDEX", "CON_USD"))) %>% 
   filter(TimePeriod >= 2010)
 
-  # TODO: Get data on regions and income levels to incorporate
+# Convert all data to a percent where 100 is good (because this makes for better viz)
 goal3_perc <- goal3_perc %>% 
-  group_by(TimePeriod, GeoAreaCode) %>% 
-  mutate(geo_mean = mean(Value)) %>% 
-  ungroup() %>% 
-  group_by(TimePeriod, Target) %>% 
-  mutate(target_mean = mean(Value)) %>% 
-  ungroup()
+  mutate(Value = 100 - Value)
 
+  # TODO: Get data on regions and income levels to incorporate
+  # TODO: Get data on foreign investment (US, China, Russia) to incorporate
 
 ################################## 2. Trends ##################################
 
@@ -90,27 +90,90 @@ lagging_targets <- goal3_perc %>%
   group_by(TimePeriod) %>% 
   mutate(world_avg = mean(Value)) %>% 
   ungroup() %>% 
+  
+  # Get the target average over time
   group_by(TimePeriod, Target) %>% 
   mutate(target_avg = mean(Value)) %>% 
   ungroup() %>% 
-  mutate(diff = world_avg - target_avg)
 
-  # Table of targets who are doing the worst
-bottom10 <- lagging_targets %>% 
+  mutate(diff = target_avg - world_avg)
+
+# Graph of difference between targets and world average
+  # TODO: incorporate indicators measured in numbers, not just percentages
+  # TODO: have this be an interactive where you can choose target, indicator, or series from the drop-down
+  # TODO: have this be an interactive where you can choose to see the top 10 or bottom 10 of the above
+  # TODO: have this be an interactive where you can choose the year
+lagging_targets %>% 
   filter(TimePeriod == 2019) %>%
   distinct(TimePeriod, Target, world_avg, target_avg, diff) %>% 
-  arrange(desc(diff)) %>% 
-  head(5)
-
-# Table of targets who are doing the best
-top10 <- lagging_targets %>% 
-  filter(TimePeriod == 2019) %>% 
-  distinct(TimePeriod, Target, world_avg, target_avg, diff) %>% 
   arrange(diff) %>% 
-  head(5)
+  head(10) %>% 
+  ggplot() +
+    geom_bar(aes(x = reorder(Target, -diff), y = diff), stat = 'identity') +
+    labs(title = "Difference between Targets and Overall Average",
+         x = "Target",
+         y = "Difference (% pts)")
+
 
 
 ################################## 4. Countries ##################################
 
+# Merge with country data
+goal3_countries <- goal3_perc %>% 
+  left_join(countries, by = c("GeoAreaName" = "Country.or.Area")) %>% 
+  drop_na("ISO-alpha3.code")
 
+
+lagging_countries <- goal3_countries %>% 
+  group_by(TimePeriod, GeoAreaCode) %>% 
+  mutate(country_avg = mean(Value)) %>% 
+  ungroup()
+
+# Table of countries who are doing the worst
+lagging_countries %>% 
+  filter(TimePeriod == 2019) %>%
+  distinct(TimePeriod, `ISO-alpha3.code`, country_avg) %>% 
+  arrange(country_avg) %>% 
+  head(10) %>% 
+  ggplot() +
+    geom_bar(aes(x = reorder(`ISO-alpha3.code`, country_avg), y = country_avg), stat = 'identity') +
+    ylim(0,100) +
+    labs(title = "Worst-Performing Countries",
+       x = "Country",
+       y = "Avg Target Score")
+
+# Table of countries who are doing the best
+lagging_countries %>% 
+  filter(TimePeriod == 2019) %>% 
+  distinct(TimePeriod, `ISO-alpha3.code`, country_avg) %>% 
+  arrange(country_avg) %>% 
+  tail(10) %>% 
+  ggplot() +
+    geom_bar(aes(x = reorder(`ISO-alpha3.code`, country_avg), y = country_avg), stat = 'identity') +
+    ylim(0,100) +
+    labs(title = "Best-Performing Countries",
+       x = "Country",
+       y = "Avg Target Score")
+
+################################## 5. Foreign Assistance ##################################
+
+
+
+################################## 6. Recommendation ##################################
+
+# Reference: https://uc-r.github.io/kmeans_clustering
+
+
+goal3_2019_countries <- goal3_countries %>% 
+  filter(TimePeriod == 2019) %>% 
+  group_by(GeoAreaCode) %>% 
+  mutate(country_avg = mean(Value, na.rm = T)) %>% 
+  ungroup() %>% 
+  distinct(SeriesCode, GeoAreaCode, GeoAreaName, `ISO-alpha3.code`, country_avg) %>% 
+  pivot_wider(names_from = SeriesCode, values_from = country_avg)
+
+# Choosing # of clusters for k-means
+fviz_nbclust(goal3_2019_countries, kmeans, method = "wss")
+fviz_nbclust(df, kmeans, method = "silhouette")
+fviz_gap_stat(gap_stat)
 
